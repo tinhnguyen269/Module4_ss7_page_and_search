b495e017b085d58cb9025458e4c3ee9befa8a236
tinhnguyen269 <tinhnguyen180507@gmail.com>
2024-07-28 08:37:13 +0700

    thuc hanh

---
+++ src/main/java/com/example/customer_manager_page_and_search/CustomerManagerPageAndSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_manager_page_and_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagerPageAndSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerManagerPageAndSearchApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/customer_manager_page_and_search/controllers/CustomerController.java
@@ -0,0 +1,2 @@
+package com.example.customer_manager_page_and_search.controllers;public class CustomerController {
+}
